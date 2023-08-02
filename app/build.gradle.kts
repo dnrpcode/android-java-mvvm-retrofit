f05472ad8837b86c7eb1bf42b5c0a2e789bca9eb
Dani Nur Ramadhan Prayogi <dani.nurramadhan@dansmultipro.com>
2023-08-02 09:30:10 +0700

    add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,23 @@ android {
 }
 
 dependencies {
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.3")
 
+    // ViewModel
+    implementation ("androidx.lifecycle:lifecycle-viewmodel:2.4.0")
+    // LiveData
+    implementation ("androidx.lifecycle:lifecycle-livedata:2.4.0")
+
+    // retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // glide
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     testImplementation("junit:junit:4.13.2")
