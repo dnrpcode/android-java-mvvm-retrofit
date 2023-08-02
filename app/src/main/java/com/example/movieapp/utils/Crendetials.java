b99b30b4ebc6db33327c28f45186dbf04aee18cd
Dani Nur Ramadhan Prayogi <dani.nurramadhan@dansmultipro.com>
2023-08-02 10:20:09 +0700

    add credentials

---
+++ app/src/main/java/com/example/movieapp/utils/Crendetials.java
@@ -0,0 +1,7 @@
+package com.example.movieapp.utils;
+
+public class Crendetials {
+    public static final String BASE_URL = "https://api.themoviedb.org/3/";
+    public static final String API_KEY = "cbc67d6a595c52fb71c050f93dc2f0fd";
+    public static final String POSTER_URL = "https://image.tmdb.org/t/p/original/";
+}
